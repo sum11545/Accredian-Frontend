1a22b5e2b32bc75ef0f3cd43665037da46f96c5f
sumit <sumitrai3252@gmail.com>
2025-02-18 02:02:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <div>
+    <h5 className='bg-blue-50 text-center mg-2 py-2'>Navigate your ideal career path with tailored expert advice <a href="" className='text-blue-400 '>  Contact Expert</a> </h5>
+    </div>
+    <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <>
+    <nav className='flex items-center justify-between p-4 shadow-md'>
+<img src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/logo.webp" alt="" className='h-8 ml-20'  />
+<div>
+  <button type='button ' >Course</button>
+  </div>
+     </nav>
+   </>
+  )
+}
+
+
+
+
+
+
+
+
+export default Navbar
